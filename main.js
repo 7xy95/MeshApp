487bbf6c460df92fb99d50f862f72e84e4362c81
7xy95 <mikhailo.bondarev@bsm.org.es>
2026-04-26 08:10:35 +0200

    contacts + quick startup
    - quick startup
    - more informative startup
    - contacts added
    - autoupdate added
    - changed 'Address' to 'Your Address'
    - font changed to monospace

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const { app, BrowserWindow } = require("electron");
-app.commandLine.appendSwitch('remote-debugging-port', '9222');
+const { updateElectronApp, UpdateSourceType } = require("update-electron-app")
 
+updateElectronApp({
+    updateSource: {
+        type: UpdateSourceType.ElectronPublicUpdateService,
+        repo: "7xy95/MeshApp"
+    },
+    updateInterval: "10 minutes"
+})
 function createWindow() {
     const win = new BrowserWindow({
         width: 700,
-        height: 700,
+        height: 710,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
             backgroundThrottling: false
         }
-
     });
     win.loadFile("index.html");
 }
-
 app.whenReady().then(createWindow);
